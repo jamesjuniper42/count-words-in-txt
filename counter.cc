47f69e524ea82ec96d58f7c855ec3b95259cdf02
jamesjuniper42 <kw690225@ohio.edu>
2025-10-16 20:50:38 -0400

    I can only count to 4

--- counter.cc
+++ counter.cc
@@ -1,1 +1,43 @@
 
+//takes a txt file, counts number of words in it
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <fstream>
+#include <cctype>
+using namespace std;
+
+void count (ifstream &inputString); //void function to count words in txt file
+
+int main(int argc, char const *argv[])
+{
+
+	ifstream inStream;
+
+	string inStreamString;
+	inStreamString = argv[1];
+
+	inStream.open(inStreamString);
+	if (inStream.fail())
+	{
+		cout << "Error: file not found" << endl;
+		exit(0);
+	}
+
+	count(inStream);
+
+	return 0;
+}
+
+void count (ifstream &inputString)
+{
+	int counter = 0;
+	while (!inputString.eof())
+	{
+		string temp;
+		inputString >> temp; //I don't know how to get it to work without making a dummy string
+		counter++;
+	}
+	cout << counter << endl;
+}
